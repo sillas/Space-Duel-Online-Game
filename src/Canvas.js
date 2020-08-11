8bcecea2a0262636a75ae83981cbfd6757ef2947
Sillas <deepsky.sill@gmail.com>
2020-08-11 01:16:05 -0300

    Basic canvas config w basic keyboard input

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,5 +1,38 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-const Canvas = () => (<h1>Olá</h1>)
+const Canvas = () => {
+
+    const canvas_ref = useRef(null)
+    const context_ref = useRef(null)
+    const winW = window.innerWidth - 3
+    const winH = window.innerHeight - 3
+
+    useEffect(() => {
+        const canvas = canvas_ref.current
+        canvas.width = winW
+        canvas.height = winH
+
+        context_ref.current = canvas.getContext('2d')
+    }, [])
+
+    const keyDown = ({nativeEvent}) => {
+        const {key} = nativeEvent
+        console.log( 'K Down', key );
+    }
+
+    const keyUp = ({nativeEvent}) => {
+        const {key} = nativeEvent
+        console.log( 'K Up', key );
+    }
+
+    return (
+        <canvas
+            onKeyDown={keyDown}
+            onKeyUp={keyUp}
+            tabIndex='0'
+            ref={canvas_ref}
+        />
+    )
+}
 
 export default Canvas
