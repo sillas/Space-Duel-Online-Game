9388e53e0fed9975d2f39dadcafd2cebb19d6e73
Sillas <deepsky.sill@gmail.com>
2020-08-11 00:55:02 -0300

    First Commit: Environment preparation

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Canvas from './Canvas';
+
+const App = () => <Canvas/>
+
+export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Canvas = () => (<h1>Olá</h1>)
+
+export default Canvas
